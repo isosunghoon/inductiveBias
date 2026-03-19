44abf3ec9c934a8ad457bd3834f564c8c1e016e7
royk999 <blobalternative@gmail.com>
2026-03-19 05:18:18 +0000

    commit jaeddae jaeddae

--- run_cka.sh
+++ run_cka.sh
@@ -5,17 +5,19 @@ BASE_CONFIG="${BASE_CONFIG:-./config/base.yaml}"
 
 # Space-separated list of per-model config yamls (same order as CKPTS)
 CONFIGS=(
-    "./config/mlpmixer.yaml"
+    # "./config/mlpmixer.yaml"
     "./config/vit.yaml"
-    "./config/poolformer.yaml"
-    # "./config/model3.yaml"
+    "./config/vit.yaml"
+    #"./config/poolformer.yaml"
+    #"./config/localvit.yaml"
 )
 
 # Space-separated list of checkpoint paths (same order as CONFIGS)
 CKPTS=(
-    "./output/base_model_exp/mlpmixer-2026-03-13_05-47-26/best.pt"
+    # "./output/base_model_exp/mlpmixer-2026-03-13_05-47-26/best.pt"
     "./output/base_model_exp/vit-2026-03-12_15-39-12/best.pt"
-    "./output/base_model_exp/poolformer-2026-03-12_23-11-35/best.pt"
+    "./output/base_model_exp/vit-2026-03-12_15-39-12/best.pt"
+    #"./output/base_model_exp/poolformer-2026-03-12_23-11-35/best.pt"
     #"./output/base_model_exp/localvit-2026-03-12_18-08-26/best.pt"
 )
 
