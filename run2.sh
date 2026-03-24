240a15b116a0d21f6b4b7a6bddafbd1c84a4d506
royk999 <blobalternative@gmail.com>
2026-03-24 01:07:52 +0000

    delete depreceted files & non-mlp config

--- run2.sh
+++
@@ -1,4 +0,0 @@
-python train.py --config config/denseformer.yaml --mixer_drop 0.1
-python train.py --config config/convformer.yaml --kernel_size 5 --run_name convformer_w5
-python train.py --config config/convformer.yaml --kernel_size 3 --run_name convformer_w3
-python train.py --config config/convformer2.yaml --run_name convformer2
