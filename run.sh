573813879719f960e3ca0c6d5862c68fe99663d3
Minseop Lee <minseopgod7@gmail.com>
2026-04-30 07:04:55 +0000

    chore: run for window size 5

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@ tsp python train.py --config config/localvit.yaml --run_name localvit --device_t
 tsp python train.py --config config/convformer.yaml --run_name convformer --device_type 0 --train_batch_size 1024
 tsp python train.py --config config/denseformer.yaml --run_name denseformer --device_type 0 --train_batch_size 1024
 tsp python train.py --config config/identity.yaml --run_name identity --device_type 0 --train_batch_size 1024
+tsp python train.py --config config/localvit.yaml --run_name localvit_w5 --device_type 0 --train_batch_size 1024 --window_size 5
+tsp python train.py --config config/convformer.yaml --run_name convformer_w5 --device_type 0 --train_batch_size 1024 --kernel_size 5
